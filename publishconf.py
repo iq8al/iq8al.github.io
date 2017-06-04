d6d1d8a78d3ee6c8352bbfe3f6a4b14f9da61368
Iqbal Abdullah <iqbal@xoxzo.com>
2017-06-04 23:28:54 +0800

    Change SITEURL

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://iq8al.github.io'
+SITEURL = 'http://iqbalabd.me'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
